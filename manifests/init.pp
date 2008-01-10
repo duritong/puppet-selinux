ad1b3e725585f7bf1f479cdba6ed560a0e09fbcd
andreas <andreas@immerda.ch>
2008-01-10 16:32:57 +0000

    immer noch probleme, evtl. doch in '' (err: /Main[top]/Node[default]/Node[immerda_default]/Node[default_gentoo]/Node[xennode]/Node[immer1-0.glei.ch]/Node[config::selinux::immerda]/Selinux::Loadmodule[logrotate]/File[/usr/share/selinux/strict/logrotate.pp]: Failed to retrieve current state of resource: undefined method `[]' for nil:NilClass)

--- manifests/init.pp
+++ manifests/init.pp
@@ -99,11 +99,11 @@ define selinux::loadmodule ($location) {
     }
 	# updates, if $location is refreshed and module already active
     file { $location:
-  	source => $location
+  	source => '$location'
     }
     exec { "SELinux-$name-Update":
                 command     => "/usr/sbin/semodule -u $location",
-                subscribe   => file[$location],
+                subscribe   => file['$location'],
                 refreshonly => true,
                 onlyif => "/usr/bin/test -e /etc/selinux/strict/modules/active/modules/$name.pp"
     }
