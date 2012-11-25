b34b4e441379ede76651aea9d8fd36af58b14af4
mh <mh@immerda.ch>
2012-11-25 17:07:53 +0100

    semanage depends on libs provided by setroubleshoot-server

--- manifests/seport.pp
+++ manifests/seport.pp
@@ -1,18 +1,21 @@
+# manage a port for selinux
+#
+# * setype: which selinux type the port should be labeled
+# * action: add/manage - Default: add
+# * port: specify the port - Default: $name
+# * protocol: tcp/udp - Default: tcp
 define selinux::seport(
   $setype,
   $action = 'add',
-  $port = 'absent',
+  $port = $name,
   $protocol = 'tcp'
 ){
-  $real_port = $port ? {
-    'absent' => $name,
-    default => $port
-  }
   $cli_action = $action ? {
     'manage' => '-m',
     default => '-a',
   }
-  exec{"semanage port ${cli_action} -t ${setype} -p ${protocol} ${real_port}":
-    unless => "semanage port -l | grep -i ${setype} | grep -qE '${protocol}[ ]+([0-9, ]+)? ${real_port}'"
+  exec{"semanage port ${cli_action} -t ${setype} -p ${protocol} ${port}":
+    unless  => "semanage port -l | grep -i ${setype} | grep -qE '${protocol}[ ]+([0-9, ]+)? ${port}'",
+    require => Package['setroubleshoot-server'],
   }
 }
