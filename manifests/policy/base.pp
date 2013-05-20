986b5ffa04a1d1313939670e257a31eddb0068b2
mh <mh@immerda.ch>
2013-05-20 22:39:32 +0200

    this package is needed for policy compiling

--- manifests/policy/base.pp
+++ manifests/policy/base.pp
@@ -1,10 +1,12 @@
 # basic things needed to deploy selinux policies
 class selinux::policy::base {
   $dir = '/var/lib/puppet/selinux_policies'
-  file{$dir:
+  package{'selinux-policy-devel':
+    ensure => installed,
+  } -> file{$dir:
     ensure => directory,
     owner  => root,
     group  => 0,
     mode   => '0600';
-  }
+  } 
 }
