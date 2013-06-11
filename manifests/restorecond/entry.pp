6b4573149b4793b12949239a12f5b876cd1e0acc
mh <mh@immerda.ch>
2013-06-11 23:30:51 +0200

    management of restorecond

---
+++ manifests/restorecond/entry.pp
@@ -0,0 +1,11 @@
+class selinux::restorecond::entry(
+  $ensure = 'present'
+){
+  include selinux::restorecond
+  file_line{"restorecond_${name}":
+    ensure => $ensure,
+    path   => '/etc/selinux/restorecond.conf',
+    line   => $name,
+    notify => Service['restorecond'],
+  }
+}
